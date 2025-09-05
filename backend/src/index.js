349be8ffbe291d035006ed8c6a9bdc1758254b7e
Supriya <supriyanovember10@gmail.com>
2025-09-05 18:11:08 +0530

    Initial setup done

---
+++ backend/src/index.js
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+dotenv.config(); // load .env before anything else
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+// Use fallback in case PORT is not set in .env
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+// Middleware
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Production build serve
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Start server
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
